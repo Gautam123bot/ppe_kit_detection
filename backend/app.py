e86d8e95405c5b40af165c84fead60bdaf695921
Gautam <0101gautakumar@gmail.com>
2024-03-04 23:25:22 +0530

    getting complete with the frontend part

---
+++ backend/app.py
@@ -0,0 +1,13 @@
+from flask import Flask, jsonify
+from flask_cors import CORS
+app = Flask(__name__)
+CORS(app, resources={r"/api/*":{"origins":"*"}})
+
+@app.route('/api/data', methods=['GET'])
+def get_data():
+    data = {
+        "message": "Hello this is api end point"
+    }
+    return jsonify(data)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=True)
